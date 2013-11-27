9a3284fc48842aadc019189c41a8e8ef0867c682
anneliulsberg <anneli@ulsberg.no>
2013-11-27 23:18:45 +0100

    Added basic html structure to the files

---
+++ header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="nb-no">
+
+<head>
+	<meta charset="utf-8">
+	<title>Oppen fotografi</title>
+</head>
+
+<body>
+	
+	<header>Oppen fotografi</header>
+	
+	<nav>
+		<ul>
+			<li><a href="#">Kontakt</a></li>
+			<li><a href="#">Pris</a></li>
+			<li><a href="#">Portefølje</a></li>
+			<li><a href="#">Blogg</a></li>			
+		</ul>
+	</nav>
+	
+	<ul id="background">
+		<li><a href="#"></a><img src="" alt=""></li>
+		<li><a href="#"></a><img src="" alt=""></li>
+		<li><a href="#"></a><img src="" alt=""></li>
+		<li><a href="#"></a><img src="" alt=""></li>
+		<li><a href="#"></a><img src="" alt=""></li>
+		<li><a href="#"></a><img src="" alt=""></li>
+		<li><a href="#"></a><img src="" alt=""></li>
+		<li><a href="#"></a><img src="" alt=""></li>
+		<li><a href="#"></a><img src="" alt=""></li>
+		<li><a href="#"></a><img src="" alt=""></li>
+		<li><a href="#"></a><img src="" alt=""></li>
+		<li><a href="#"></a><img src="" alt=""></li>
+		<li><a href="#"></a><img src="" alt=""></li>
+		<li><a href="#"></a><img src="" alt=""></li>
+		<li><a href="#"></a><img src="" alt=""></li>
+		<li><a href="#"></a><img src="" alt=""></li>
+		<li><a href="#"></a><img src="" alt=""></li>
+		<li><a href="#"></a><img src="" alt=""></li>
+		<li><a href="#"></a><img src="" alt=""></li>
+		<li><a href="#"></a><img src="" alt=""></li>
+		<li><a href="#"></a><img src="" alt=""></li>
+		<li><a href="#"></a><img src="" alt=""></li>
+		<li><a href="#"></a><img src="" alt=""></li>
+		<li><a href="#"></a><img src="" alt=""></li>
+	</ul>
+	
+	
---
+++ index.php
@@ -0,0 +1,8 @@
+<ul id="slideshow">
+	<li><a href="#"></a><img src="" alt=""></li>
+	<li><a href="#"></a><img src="" alt=""></li>
+	<li><a href="#"></a><img src="" alt=""></li>
+	<li><a href="#"></a><img src="" alt=""></li>
+	<li><a href="#"></a><img src="" alt=""></li>
+	<li><a href="#"></a><img src="" alt=""></li>
+</ul>
