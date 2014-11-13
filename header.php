eb18870ee84151f4dabdced964b7303f27cc3cba
anneliulsberg <anneli@ulsberg.no>
2014-11-13 20:42:48 +0100

    Added stylesheets to html

--- header.php
+++ header.php
@@ -1,9 +1,13 @@
+<?php $t_uri = get_template_directory_uri(); ?>
 <!DOCTYPE html>
 <html lang="nb-no">
 
 <head>
 	<meta charset="utf-8">
 	<title><?php wp_title('-', true, 'right'); ?> <?php bloginfo('name') ?></title>
+	<link rel="stylesheet" href="<?php echo $t_uri ?>/css/phone.css" media="only screen and (min-width: 320px) and (max-width: 767px)">
+	<link rel="stylesheet" href="<?php echo $t_uri ?>/css/tablet.css" media="only screen and (min-width: 768px) and (max-width: 959px)">
+	<link rel="stylesheet" href="<?php echo $t_uri ?>/css/desktop.css" media="only screen and (min-width: 960px)">
 	<?php wp_head(); ?>
 </head>
 
