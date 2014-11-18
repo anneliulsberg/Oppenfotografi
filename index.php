8ac79d10fecc41c2adc74564e86c3d854a43c8a4
anneliulsberg <anneli@ulsberg.no>
2014-11-18 22:44:07 +0100

    Removed img from slideshow

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php get_header(); ?>
 
 <ol id="slideshow">
-	<li><a href="#"></a><img src="" alt=""></li>
-	<li><a href="#"></a><img src="" alt=""></li>
-	<li><a href="#"></a><img src="" alt=""></li>
-	<li><a href="#"></a><img src="" alt=""></li>
-	<li><a href="#"></a><img src="" alt=""></li>
-	<li><a href="#"></a><img src="" alt=""></li>
+	<li><a href="#"></a></li>
+	<li><a href="#"></a></li>
+	<li><a href="#"></a></li>
+	<li><a href="#"></a></li>
+	<li><a href="#"></a></li>
+	<li><a href="#"></a></li>
 </ol>
 
 <?php get_footer(); ?>
