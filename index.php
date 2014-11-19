99195d5a83003025cf6cd8e55a538e6581636a82
anneliulsberg <anneli@ulsberg.no>
2014-11-19 22:28:33 +0100

    Added slideshow titles

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php get_header(); ?>
 
 <ol id="slideshow">
-	<li><a href="#"></a></li>
-	<li><a href="#"></a></li>
-	<li><a href="#"></a></li>
-	<li><a href="#"></a></li>
-	<li><a href="#"></a></li>
-	<li><a href="#"></a></li>
+	<li><a href="#"><span>Kunstfotografi</span></a></li>
+	<li><a href="#"><span>Studio portrett</span></a></li>
+	<li><a href="#"><span>Redaksjonelt: Portrett</span></a></li>
+	<li><a href="#"><span>Mote</span></a></li>
+	<li><a href="#"><span>Bedrift</span></a></li>
+	<li><a href="#"><span>Akt</span></a></li>
 </ol>
 
 <?php get_footer(); ?>
