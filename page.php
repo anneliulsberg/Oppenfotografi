55173dbc5dc8e5d37586f512e8fe6def3a578e59
anneliulsberg <anneli@ulsberg.no>
2015-02-23 22:16:58 +0100

    Wrapped article in page

--- page.php
+++ page.php
@@ -2,11 +2,13 @@
 	
 	<?php while (have_posts()) : the_post(); ?>
 	
-		<article>
-			<h1><?php the_title() ?></h1>
-			<?php the_content() ?>	
-		</article>	
-	
+		<div id="page">
+			<article>
+				<h1><?php the_title() ?></h1>
+				<?php the_content() ?>	
+			</article>	
+		</div>
+
 	<?php endwhile; ?>
 
 <?php get_footer(); ?>
