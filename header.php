2ab6026604a650207a3d56975011cad6a98a5080
anneliulsberg <anneli@ulsberg.no>
2015-02-23 23:46:27 +0100

    Replaced static menu with wp_nav_menu

--- header.php
+++ header.php
@@ -22,11 +22,6 @@
 	
 		<div class="wrapper">
 			<nav>
-				<ul>
-					<li id="contact"><a href="/kontakt">Kontakt</a></li>
-					<li id="price"><a href="#">Pris</a></li>
-					<li id="portfolio"><a href="#">Portefølje</a></li>
-					<li id="blog"><a href="#">Blogg</a></li>			
-				</ul>
+				<?php wp_nav_menu(array('container' => false)); ?>
 			</nav>
 		</div>
