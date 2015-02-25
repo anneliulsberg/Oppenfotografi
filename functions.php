474906a01d59121c002ec699cea4db4188ac90bb
anneliulsberg <anneli@ulsberg.no>
2015-02-25 23:07:20 +0100

    added body class filter

--- functions.php
+++ functions.php
@@ -30,10 +30,22 @@ function oppen_nav_menu_item_id($menu_id, $item, $args, $depth) {
 	return $menu_id;
 }
 
+function oppen_body_class($classes) {
+	if (is_category()) {
+		$category_id = get_query_var('cat');
+		$category = get_category($category_id);
+		return array($category->slug);
+	}	else if (is_page()) {
+		$post = get_post($item->object_id);
+		return array($post->post_name);
+	}
+	
+	return $classes;
+}
+
+add_filter('body_class', 'oppen_body_class', 10, 2);
 add_filter('nav_menu_item_id', 'oppen_nav_menu_item_id', 10, 2);
-
 add_filter('nav_menu_css_class' , 'oppen_nav_menu_item_css_class', 10, 2);
-
 add_action('after_setup_theme', 'oppenfotografi_setup');
 
 ?>
