253c1dd1b7a7d29eaa764e5dfae90c03cb759341
anneliulsberg <anneli@ulsberg.no>
2015-03-18 22:20:10 +0100

    Added separate markup for non blog articles
    Moved most php code to the top, introduced a $is_category_blog variable
    and added separate markup for non blog articles, also added a page
    wrapper.

--- category.php
+++ category.php
@@ -1,44 +1,56 @@
-<?php get_header(); ?>
-<?php query_posts($query_string . "&orderby=title&order=asc"); ?>
+<?php
+get_header();
+query_posts($query_string . "&orderby=title&order=asc");
+$current_category_id = get_query_var('cat');
+$current_category = get_category($current_category_id);
+$is_category_blog = $current_category && $current_category->slug == 'blogg';
+$categories = get_categories(array(
+	'parent' => $current_category_id,
+	'hide_empty' => 0,
+	'orderby' => 'name',
+));
+?>
 
 <section class="category">
 	
 	<div id="page">
-	
-	<ul id="portfolio-menu">
-		<?php 
-				$current_category_id = get_query_var('cat');
-				$current_category = get_category($category_id);
-				$categories = get_categories(array(
-					'parent' => $current_category_id,
-					'hide_empty' => 0,
-					'orderby' => 'name',
-				));
+		<div class="wrapper">
+			<ul id="portfolio-menu">
+				<?php 
+						if (!$is_category_blog) :
+							foreach ($categories as $category) :
+								echo '<li class="' . esc_attr($category->slug) . '"><a href="' . get_category_link($category->term_id) . '"><span>' . $category->name . '</span></a></li>';
+							endforeach;
+				 		endif;
+				  ?>
+			</ul>
+			
+				<?php 
 				
-				if ($current_category && $current_category->slug != 'blogg') {
-					foreach ($categories as $category) {
-						echo '<li class="' . $category->slug . '"><a href="' . get_category_link($category->term_id) . '"><span>' . $category->name . '</span></a></li>';
-					}
-		 		}
-		  ?>
-	</ul>
-	
-		<?php 
+				if (have_posts()) :
+				
+					while (have_posts()) :
+						the_post();
 		
-		if (have_posts()) :
-		
-			while (have_posts()) :
-				the_post();
-				get_template_part('content');
-			endwhile;
-		
-		else :
-		
-			get_template_part('content', 'none'); 
-		
-		endif;
-		 
-		 ?>
+						if ($is_category_blog) :		
+								get_template_part('content');
+						else : ?>
+							<article>
+								<a href="<?php echo get_permalink(); ?>" style="background-image: ...">
+									<span><?php the_title(); ?></span>
+								</a>
+							</article>					
+						<?php endif;
+					endwhile;
+					
+				else :
+				
+					get_template_part('content', 'none'); 
+				
+				endif;
+				 
+				 ?>
+		</div>
 	</div>
 </section>
 
