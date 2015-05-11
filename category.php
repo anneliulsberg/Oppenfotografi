0c2be77849370ac89bc5aeaf63d3f1459b131404
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2015-05-11 22:34:55 +0200

    Order blog posts by date, not by name

--- category.php
+++ category.php
@@ -4,10 +4,11 @@ query_posts($query_string . "&orderby=title&order=asc");
 $current_category_id = get_query_var('cat');
 $current_category = get_category($current_category_id);
 $is_category_blog = $current_category && $current_category->slug == 'blogg';
+$orderby = $is_category_blog ? 'date' : 'name';
 $categories = get_categories(array(
 	'parent' => $current_category_id,
 	'hide_empty' => 0,
-	'orderby' => 'name',
+	'orderby' => $orderby,
 ));
 ?>
 
@@ -17,11 +18,11 @@ $categories = get_categories(array(
 		<div class="wrapper">
 			<ul id="portfolio-menu">
 				<?php 
-						if (!$is_category_blog) :
-							foreach ($categories as $category) :
-								echo '<li class="' . esc_attr($category->slug) . '"><a href="' . get_category_link($category->term_id) . '"><span>' . $category->name . '</span></a></li>';
-							endforeach;
-				 		endif;
+					if (!$is_category_blog) :
+						foreach ($categories as $category) :
+							echo '<li class="' . esc_attr($category->slug) . '"><a href="' . get_category_link($category->term_id) . '"><span>' . $category->name . '</span></a></li>';
+						endforeach;
+			 		endif;
 				?>
 			</ul>
 			
