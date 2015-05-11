ae1e535c561d68d4cdf2e6592c75ab9d492f4667
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2015-05-11 22:54:57 +0200

    Display the excerpt on all blog posts

--- content.php
+++ content.php
@@ -1,6 +1,4 @@
 <article>
 		<h1><a href="<?php the_permalink(); ?>" class="permalink"><?php the_title(); ?></a></h1>
-		<?php if ($wp_query->current_post == 0) : ?>
-			<?php the_content(); ?>
-		<?php endif; ?>
+		<?php the_excerpt(); ?>
 </article>
