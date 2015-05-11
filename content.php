2acdfbeb6ecabd4c9f80e5010e46cc8bf4841c4a
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2015-05-11 23:32:21 +0200

    Treat the first and all other blog post different
    The first blog post should have all content (without images) shown and
    the extracted first attachment image in 600 x 600 px with all other
    blog posts only showing excerpt and images in 300 x 300 px.

--- content.php
+++ content.php
@@ -1,4 +1,8 @@
-<?php $image_src = oppen_get_first_attached_image_src(array(150, 150)); ?>
+<?php
+$first_post = $wp_query->current_post == 0;
+$size = $first_post ? array(600, 600) : array(300, 300);
+$image_src = oppen_get_first_attached_image_src($size);
+?>
 <article>
 		<h1><a href="<?php the_permalink(); ?>" class="permalink"><?php the_title(); ?></a></h1>
 		<div class="metadata">
@@ -6,8 +10,21 @@
 		</div>
 		<?php if ($image_src) : ?>
 			<div class="picture">
-				<img src="<?php echo $image_src ?>" width="150" height="150" alt="">
+				<img src="<?php echo $image_src ?>" width="<?php echo $size[0] ?>" height="<?php echo $size[1] ?>" alt="">
 			</div>
 		<?php endif; ?>
-		<?php the_excerpt(); ?>
+		<?php
+
+		if ($first_post) :
+			// TODO: Bleuch, this is ugly. Find a cleaner way to get the image-less content. @asbjornu
+			$content = get_the_content();
+			$content = preg_replace("/<img[^>]+\>/i", " ", $content);          
+			$content = apply_filters('the_content', $content);
+			$content = str_replace(']]>', ']]>', $content);
+			echo $content;
+		else : 
+			the_excerpt();
+		endif;
+
+		?>
 </article>
