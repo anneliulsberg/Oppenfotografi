b01d8d9f1cdc2b39d3214c579ce6cae5bdd8e437
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2015-05-11 23:03:58 +0200

    Output the first attached image in blog posts

--- content.php
+++ content.php
@@ -1,4 +1,10 @@
+<?php $image_src = oppen_get_first_attached_image_src(array(150, 150)); ?>
 <article>
 		<h1><a href="<?php the_permalink(); ?>" class="permalink"><?php the_title(); ?></a></h1>
+		<?php if ($image_src) : ?>
+			<div class="picture">
+				<img src="<?php echo $image_src ?>" width="150" height="150" alt="">
+			</div>
+		<?php endif; ?>
 		<?php the_excerpt(); ?>
 </article>
