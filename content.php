2a0f2e97bc03238b3374e8cd97ea89a5970d2b95
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2015-05-11 23:21:16 +0200

    Added post date metadata

--- content.php
+++ content.php
@@ -1,6 +1,9 @@
 <?php $image_src = oppen_get_first_attached_image_src(array(150, 150)); ?>
 <article>
 		<h1><a href="<?php the_permalink(); ?>" class="permalink"><?php the_title(); ?></a></h1>
+		<div class="metadata">
+			<?php echo date_i18n(get_option('date_format'), strtotime($post->post_date)); ?>
+		</div>
 		<?php if ($image_src) : ?>
 			<div class="picture">
 				<img src="<?php echo $image_src ?>" width="150" height="150" alt="">
