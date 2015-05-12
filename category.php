11e7b4c004204532561351911548d2cbca7550bd
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2015-05-12 22:13:07 +0200

    The top-level category list should show on sub categories
    Refactored category.php so the top-level category list shows on sub
    categories with the use of oppen_get_current_categories().

--- category.php
+++ category.php
@@ -1,12 +1,15 @@
 <?php
 get_header();
-$current_category_id = get_query_var('cat');
-$current_category = get_category($current_category_id);
+
+$current_categories = oppen_get_current_categories();
+$current_category = $current_categories[0];
 $is_category_blog = $current_category && $current_category->slug == 'blogg';
+$is_sub_category = count($current_categories) > 1;
 $orderby = $is_category_blog ? '&orderby=date&order=desc' : '&orderby=title&order=asc';
 query_posts($query_string . $orderby);
-$categories = get_categories(array(
-	'parent' => $current_category_id,
+$parent_category_id = $current_categories[count($current_categories) - 1]->cat_ID;
+$top_level_categories = get_categories(array(
+	'parent' => $parent_category_id,
 	'hide_empty' => 0,
 	'orderby' => 'name',
 ));
@@ -14,13 +17,12 @@ $categories = get_categories(array(
 ?>
 
 <section class="category">
-	
 	<div id="page">
 		<div class="wrapper">
 			<ul id="portfolio-menu">
 				<?php 
 					if (!$is_category_blog) :
-						foreach ($categories as $category) :
+						foreach ($top_level_categories as $category) :
 							echo '<li class="' . esc_attr($category->slug) . '"><a href="' . get_category_link($category->term_id) . '"><span>' . $category->name . '</span></a></li>';
 						endforeach;
 			 		endif;
@@ -35,7 +37,7 @@ $categories = get_categories(array(
 	
 					if ($is_category_blog) :		
 							get_template_part('content');
-					else :
+					elseif ($is_sub_category) :
 						$image_src = oppen_get_first_attached_image_src(array(150, 150)); ?>
 						<article>
 							<a href="<?php echo get_permalink(); ?>" <?php echo $image_src ? "style='background-image: url($image_src)'" : '' ?>>
