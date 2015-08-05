98eec0eb707588708778b85504713a7edab91f51
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2015-08-06 00:21:51 +0200

    Simplify the content for the blog, using the new thumbnail size

--- content.php
+++ content.php
@@ -1,30 +1,9 @@
-<?php
-$first_post = $wp_query->current_post == 0;
-$size = $first_post ? array(600, 600) : array(300, 300);
-$image_src = oppen_get_first_attached_image_src($size);
-?>
 <article>
-		<h1><a href="<?php the_permalink(); ?>" class="permalink"><?php the_title(); ?></a></h1>
-		<div class="metadata">
-			<?php echo date_i18n(get_option('date_format'), strtotime($post->post_date)); ?>
-		</div>
-		<?php if ($image_src) : ?>
-			<div class="picture">
-				<img src="<?php echo $image_src ?>" alt="">
-			</div>
-		<?php endif; ?>
-		<?php
-
-		if ($first_post) :
-			// TODO: Bleuch, this is ugly. Find a cleaner way to get the image-less content. @asbjornu
-			$content = get_the_content();
-			$content = preg_replace("/<img[^>]+\>/i", " ", $content);          
-			$content = apply_filters('the_content', $content);
-			$content = str_replace(']]>', ']]>', $content);
-			echo $content;
-		else : 
-			the_excerpt();
-		endif;
-
-		?>
+    <h1><a href="<?php the_permalink(); ?>" class="permalink"><?php the_title(); ?></a></h1>
+    <div class="metadata">
+        <?php echo date_i18n(get_option('date_format'), strtotime($post->post_date)); ?>
+    </div>
+    <div class="picture">
+        <?php the_post_thumbnail('thumbnail') ?>
+    </div>
 </article>
