23f253aa54d7f5411e4c91d8c8430c01e1d83c71
anneliulsberg <anneli@ulsberg.no>
2015-09-07 22:10:43 +0200

    Removed media queries
    And renamed the css file

--- header.php
+++ header.php
@@ -5,9 +5,7 @@
 <head>
 	<meta charset="utf-8">
 	<title><?php wp_title('-', true, 'right'); ?> <?php bloginfo('name') ?></title>
-	<link rel="stylesheet" href="<?php echo $t_uri ?>/css/phone.css" media="only screen and (min-width: 320px) and (max-width: 767px)">
-	<link rel="stylesheet" href="<?php echo $t_uri ?>/css/tablet.css" media="only screen and (min-width: 768px) and (max-width: 959px)">
-	<link rel="stylesheet" href="<?php echo $t_uri ?>/css/desktop.css" media="only screen and (min-width: 960px)">
+	<link rel="stylesheet" href="<?php echo $t_uri ?>/css/style.css">
 	<link href='http://fonts.googleapis.com/css?family=Economica:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 	<script src="<?php echo $t_uri ?>/js/jquery.bgswitcher.js" type="text/javascript"></script>
