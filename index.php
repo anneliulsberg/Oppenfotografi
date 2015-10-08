d92275c3a82a258d4fc60548f2581cba9ad43027
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2015-10-08 23:43:08 +0200

    Move the bootstrapping code up
    Move the bootstrapping code for fetching categories for the portfolio
    menu up to the top of the file.

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
+<?php
+$portfolio_category = get_category_by_slug('portefolje');
+$model_category = get_category_by_slug('modellportefolje');
+$categories = get_categories(array(
+    'parent' => $portfolio_category->term_id,
+    'exclude' => $model_category->term_id
+));
+?>
 <?php get_header(); ?>
 
     <div class="wrapper">
         <ol id="slideshow">
-            <?php
-                $portfolio_category = get_category_by_slug('portefolje');
-                $model_category = get_category_by_slug('modellportefolje');
-                $categories = get_categories(array(
-                    'parent' => $portfolio_category->term_id,
-                    'exclude' => $model_category->term_id
-                ));
-            ?>
             <?php foreach ($categories as $category) : $category_url = get_category_link($category->term_id); ?>
 
                 <li><a href="<?php echo esc_attr($category_url) ?>"><span><?php echo $category->name ?></span></a></li>
