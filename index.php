76a1f33084c5ebb6f3f5c86168c30105620c4490
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2015-10-10 23:47:16 +0200

    CSS animations with 4 images was hard. Let's get it to work with 2 first.

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ $categories = get_categories(array(
                                 on			rel.`object_id`					= post.`ID`
                                 and			rel.`term_taxonomy_id`			= $category->term_id
                             where 			attachment.`post_type` 			= 'attachment'
-                            limit 			4
+                            limit 			2
                         ";
 
                         $images = $wpdb->get_results($wpdb->prepare($query));
