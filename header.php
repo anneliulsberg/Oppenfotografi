e98228a1d6ed65eb53b1d08545852faeaccff26c
anneliulsberg <anneli@ulsberg.no>
2015-10-11 00:05:22 +0200

    Replaced js slideshow with css animation

--- header.php
+++ header.php
@@ -8,10 +8,6 @@
 	<title><?php wp_title('-', true, 'right'); ?> <?php bloginfo('name') ?></title>
 	<link rel="stylesheet" href="<?php echo $t_uri ?>/css/style.css">
 	<link href='http://fonts.googleapis.com/css?family=Economica:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-	<script src="<?php echo $t_uri ?>/js/jquery.bgswitcher.js" type="text/javascript"></script>
-	<script type="text/javascript">var baseUri = '<?php echo $t_uri ?>';</script>
-	<script src="<?php echo $t_uri ?>/js/main.js" type="text/javascript"></script>
 	<?php wp_head(); ?>
 </head>
 
